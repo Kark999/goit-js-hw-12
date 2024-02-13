9eaea6ccf7c19c57e7ec52fd10b13be332b5076c
Andrey Svyrskyi <akkad1v@gmail.com>
2024-02-13 20:47:49 +0200

    'update'

--- src/main.js
+++ src/main.js
@@ -1,10 +1,7 @@
-import iziToast from 'izitoast';
-import 'izitoast/dist/css/iziToast.min.css';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
-import errorIcon from '../src/img/octagon.svg';
-import closeIcon from '../src/img/close.svg';
-import axios from 'axios';
+import { getImagesByType } from './js/pixabay-api';
+import * as render from './js/render-functions';
 
 const refs = {
   form: document.querySelector('.form'),
@@ -12,153 +9,73 @@ const refs = {
   loader: document.querySelector('.loader'),
   searchMore: document.querySelector('.search-more'),
 };
-let gallery = new SimpleLightbox('.gallery a');
+const params = {
+  key: '42141224-180b0a56c10fd436e302d680a',
+  q: '',
+  image_type: 'photo',
+  orientation: 'horizontal',
+  safesearch: true,
+  page: '',
+  per_page: 15,
+};
 
-let query = null;
-let page = null;
-const per_page = 15;
+let gallery = new SimpleLightbox('.gallery a');
 
 refs.form.addEventListener('submit', onFormSubmit);
 refs.searchMore.addEventListener('click', onGetImageByPage);
 
 async function onGetImageByPage(e) {
-  e.preventDefault();
-  page += 1;
+  params.page += 1;
 
   refs.loader.classList.remove('hidden');
   try {
-    const data = await getImagesByType();
+    const data = await getImagesByType(params);
     if (data.totalHits === 0) {
-      return showError(message);
+      return render.showError(message); // check and change
     }
-    const markup = galleryTemplate(data.hits);
+    const markup = render.galleryTemplate(data.hits);
     refs.gallery.insertAdjacentHTML('beforeend', markup);
     gallery.refresh();
-    checkBtnStatus(data);
+    render.checkBtnStatus(data, params, refs.searchMore);
   } catch (error) {
-    showError(error);
+    render.showError(error); // check and change
   }
   refs.loader.classList.add('hidden');
-  smoothScroll();
+  render.smoothScroll(refs.gallery.firstElementChild);
 }
 
 async function onFormSubmit(e) {
   e.preventDefault();
-  query = e.target.elements.search.value.trim();
-  page = 1;
-
-  refs.loader.classList.remove('hidden');
+  params.q = e.target.elements.search.value;
+  params.page = 1;
   refs.gallery.innerHTML = '';
-  try {
-    const data = await getImagesByType();
-    if (data.totalHits === 0) {
-      refs.loader.classList.add('hidden');
-      refs.searchMore.classList.add('hidden');
-      return showError(
-        'Sorry, there are no images matching <br/> your search query. Please try again!'
-      );
-    }
-    const markup = galleryTemplate(data.hits);
-    refs.gallery.innerHTML = markup;
-    gallery.refresh();
-    checkBtnStatus(data);
-  } catch (error) {
-    showError(error);
-  }
-  refs.loader.classList.add('hidden');
-  refs.form.reset();
-}
-
-async function getImagesByType() {
-  const url = 'https://pixabay.com/api/';
-  const response = await axios.get(url, {
-    params: {
-      key: '42141224-180b0a56c10fd436e302d680a',
-      q: query,
-      image_type: 'photo',
-      orientation: 'horizontal',
-      safesearch: true,
-      page: page,
-      per_page: per_page,
-    },
-  });
-  return response.data;
-}
-
-function imageTemplate(data) {
-  return `<li class="gallery-card">
-        <a href="${data.largeImageURL}"
-          ><img
-            class="gallery-image"
-            src="${data.webformatURL}"
-            alt="${data.tags}"
-            title=""
-          />
-        </a>
-        <div class="gallery-card-items">
-          <p class="gallery-card-info">
-            Likes
-            <span class="gallery-card-data">${data.likes}</span>
-          </p>
-
-          <p class="gallery-card-info">
-            Views
-            <span class="gallery-card-data">${data.views}</span>
-          </p>
-
-          <p class="gallery-card-info">
-            Comments
-            <span class="gallery-card-data">${data.comments}</span>
-          </p>
-
-          <p class="gallery-card-info">
-            Downloads
-            <span class="gallery-card-data">${data.downloads}</span>
-          </p>
-        </div>
-      </li>`;
-}
-function galleryTemplate(data) {
-  return data.map(imageTemplate).join('');
-}
-
-function showError(message) {
-  iziToast.error({
-    message,
-    position: 'topRight',
-    messageColor: '#ffffff',
-    messageSize: '16px',
-    backgroundColor: '#ef4040',
-    iconColor: '#ffffff',
-    iconUrl: errorIcon,
-    timeout: 5000,
-    close: false,
-    closeOnEscape: true,
-    buttons: [
-      [
-        `<button type="button" style="background-color: transparent" ><img src=${closeIcon}></button>`,
-        function (instance, toast) {
-          instance.hide({ transitionOut: 'fadeOut' }, toast);
-        },
-      ],
-    ],
-  });
-}
-function checkBtnStatus(data) {
-  const maxPage = Math.ceil(data.totalHits / per_page);
-  const isLastPage = maxPage === page;
-  if (isLastPage) {
-    refs.searchMore.classList.add('hidden');
-    showError("We're sorry, but you've reached the end of search results.");
+  refs.searchMore.classList.add('hidden');
+  console.log(params.q.trim());
+  if (!params.q.trim()) {
+    render.showError('digital world is mess'); // check and change
   } else {
-    refs.searchMore.classList.remove('hidden');
+    refs.loader.classList.remove('hidden');
+    try {
+      const data = await getImagesByType(params);
+      console.log(data.totalHits);
+      if (data.totalHits === 0) {
+        refs.loader.classList.add('hidden');
+        render.showError(
+          'Sorry, there are no images matching <br/> your search query. Please try again!'
+        );
+        return;
+      }
+      const markup = render.galleryTemplate(data.hits);
+      refs.gallery.innerHTML = markup;
+      gallery.refresh();
+      render.checkBtnStatus(data, params, refs.searchMore);
+      // const checkBtnStatusBound = render.checkBtnStatus.bind(refs);
+      // checkBtnStatusBound(data, params);
+    } catch (error) {
+      render.showError(error); // check and change
+      console.log(error);
+    }
+    refs.loader.classList.add('hidden');
   }
-}
-function smoothScroll() {
-  const card = refs.gallery.firstElementChild;
-  const cardSize = card.getBoundingClientRect();
-  scrollBy({
-    behavior: 'smooth',
-    top: cardSize.height * 2,
-  });
+  refs.form.reset();
 }
